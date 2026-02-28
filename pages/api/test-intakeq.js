ef0d6698afa8e07c1a80fdd5c223a27ec51c8cd1
HealingSoulutions <info@healingsoulutions.care>
2026-02-28 13:33:57 -0500

    Update test-intakeq.js

--- pages/api/test-intakeq.js
+++ pages/api/test-intakeq.js
@@ -36,7 +36,8 @@ export default async function handler(req, res) {
     out.client = { status: c.s, id: cid, email: em };
 
     if (cid) {
-      var i = await iq('/intakes/send', 'POST', {
+      // Test A: /intakes/send WITH Questions (what we want to work)
+      var payloadA = {
         QuestionnaireId: QID,
         PractitionerId: PRAC,
         ClientId: cid,
@@ -45,52 +46,62 @@ export default async function handler(req, res) {
         Questions: [
           { Id: 'kj1o-1', Text: 'First name', Answer: 'DiagTest', QuestionType: 'OpenQuestion' },
           { Id: 'oj9c-1', Text: 'Last name', Answer: 'Run' + uid, QuestionType: 'OpenQuestion' },
-          { Id: '9r2z-1', Text: 'Date of birth', Answer: '06/20/1985', QuestionType: 'OpenQuestion' },
-          { Id: '9lt7-1', Text: 'Email', Answer: em, QuestionType: 'OpenQuestion' },
-          { Id: '8mqt-1', Text: 'Phone', Answer: '+1 585 747 2215', QuestionType: 'OpenQuestion' },
-          { Id: 'jhym-1', Text: 'Address line 1', Answer: '456 Test Ave', QuestionType: 'OpenQuestion' },
-          { Id: 'wt5a-1', Text: 'Address line 2', Answer: '', QuestionType: 'OpenQuestion' },
-          { Id: '9uoi-1', Text: 'State', Answer: 'NY', QuestionType: 'OpenQuestion' },
-          { Id: 'lp5z-1', Text: 'Zipcode', Answer: '14620', QuestionType: 'OpenQuestion' },
-          { Id: 'jo66-1', Text: 'Medical/surgical history', Answer: 'TEST: Appendectomy 2010', QuestionType: 'OpenQuestion' },
-          { Id: 'gkmh-1', Text: 'Current medication/supplements', Answer: 'TEST: Lisinopril 10mg', QuestionType: 'OpenQuestion' },
-          { Id: 'elrp-1', Text: 'Allergies', Answer: 'TEST: Penicillin', QuestionType: 'OpenQuestion' },
-          { Id: 'abjd-1', Text: 'Previous reaction to IV therapy?', Answer: 'TEST: None', QuestionType: 'OpenQuestion' },
-          { Id: 'andp-1', Text: 'Additional notes for clinician', Answer: 'DIAGNOSTIC - ignore', QuestionType: 'OpenQuestion' },
-          { Id: 'uvgy-1', Text: 'Additional notes', Answer: 'TEST booking', QuestionType: 'OpenQuestion' },
-          { Id: 'knxl-1', Text: 'Appt', Answer: 'Feb 28 2026 3PM IV Hydration', QuestionType: 'OpenQuestion' },
-          { Id: 't06w-1', Text: 'Consents', Answer: 'All 4 consents AGREED', QuestionType: 'OpenQuestion' },
-          { Id: 'ns11-1', Text: 'Sigs', Answer: 'Typed sig: DiagTest | Visa 4242', QuestionType: 'OpenQuestion' }
+          { Id: 'jo66-1', Text: 'Medical/surgical history', Answer: 'TEST: Appendectomy 2010', QuestionType: 'OpenQuestion' }
         ]
-      });
+      };
+      var rA = await iq('/intakes/send', 'POST', payloadA);
+      out.testA_send_with_questions = { status: rA.s, ok: rA.ok, fullResponse: rA.d };
 
-      var iid = i.d && i.d.Id ? i.d.Id : null;
-      var qs = i.d && i.d.Questions ? i.d.Questions : [];
-      var filled = 0;
-      var ansArr = [];
-      for (var x = 0; x < qs.length; x++) {
-        if (qs[x].Answer) { filled++; }
-        ansArr.push({ id: qs[x].Id, ans: qs[x].Answer });
-      }
-      out.intake = { status: i.s, ok: i.ok, id: iid, intakeStatus: i.d ? i.d.Status : null, url: i.d ? i.d.Url : null, filled: filled, total: qs.length, answers: ansArr };
+      // Test B: /intakes/send WITHOUT Questions (just send the form link)
+      var em2 = 'diag2-' + uid + '@healingsoulutions.care';
+      var c2 = await iq('/clients', 'POST', { FirstName: 'DiagB', LastName: 'Run' + uid, Email: em2, PractitionerId: PRAC });
+      var cid2 = c2.ok ? (c2.d.ClientId || c2.d.Id) : null;
+      if (cid2) {
+        var payloadB = {
+          QuestionnaireId: QID,
+          PractitionerId: PRAC,
+          ClientId: cid2,
+          ClientEmail: em2,
+          ClientName: 'DiagB Run' + uid
+        };
+        var rB = await iq('/intakes/send', 'POST', payloadB);
+        out.testB_send_no_questions = { status: rB.s, ok: rB.ok, hasId: !!(rB.d && rB.d.Id), intakeId: rB.d ? rB.d.Id : null, intakeStatus: rB.d ? rB.d.Status : null };
 
-      if (iid) {
-        var rb = await iq('/intakes/' + iid);
-        var q3 = rb.d && rb.d.Questions ? rb.d.Questions : [];
-        var f3 = 0;
-        var a3 = [];
-        for (var y = 0; y < q3.length; y++) {
-          if (q3[y].Answer) { f3++; }
-          a3.push({ id: q3[y].Id, ans: q3[y].Answer });
+        // If B worked, try updating that intake with answers
+        if (rB.ok && rB.d && rB.d.Id) {
+          var intakeId = rB.d.Id;
+
+          // Test C: POST answers to the intake
+          var rC = await iq('/intakes/' + intakeId, 'POST', {
+            Id: intakeId,
+            Questions: [
+              { Id: 'kj1o-1', Answer: 'DiagB' },
+              { Id: 'oj9c-1', Answer: 'Run' + uid },
+              { Id: 'jo66-1', Answer: 'TEST: Appendectomy 2010' }
+            ]
+          });
+          out.testC_post_answers_to_intake = { status: rC.s, ok: rC.ok, fullResponse: rC.d };
+
+          // Test D: Read it back
+          var rD = await iq('/intakes/' + intakeId);
+          var q4 = rD.d && rD.d.Questions ? rD.d.Questions : [];
+          var filled = 0;
+          var answers = [];
+          for (var x = 0; x < q4.length; x++) {
+            if (q4[x].Answer) { filled++; }
+            answers.push({ id: q4[x].Id, text: q4[x].Text, answer: q4[x].Answer });
+          }
+          out.testD_readback = { status: rD.s, intakeStatus: rD.d ? rD.d.Status : null, filled: filled, total: q4.length, answers: answers };
         }
-        out.readback = { status: rb.s, intakeStatus: rb.d ? rb.d.Status : null, filled: f3, total: q3.length, answers: a3 };
-
-        var mc = await iq('/intakes/' + iid, 'POST', { Id: iid, Status: 'Completed' });
-        out.markComplete = { status: mc.s, ok: mc.ok, response: mc.d };
       }
     }
 
-    out.SUMMARY = { clientId: cid, intakeId: out.intake ? out.intake.id : 'NONE', answersSent: out.intake ? out.intake.filled : 0, answersReadBack: out.readback ? out.readback.filled : 'N/A', completeStatus: out.markComplete ? out.markComplete.status : 'N/A' };
+    out.SUMMARY = {
+      sendWithQuestions: out.testA_send_with_questions ? out.testA_send_with_questions.status : 'N/A',
+      sendWithoutQuestions: out.testB_send_no_questions ? out.testB_send_no_questions.status : 'N/A',
+      postAnswersToIntake: out.testC_post_answers_to_intake ? out.testC_post_answers_to_intake.status : 'N/A',
+      answersReadBack: out.testD_readback ? out.testD_readback.filled : 'N/A'
+    };
     return res.status(200).json(out);
   } catch (err) {
     return res.status(200).json({ CRASHED: true, error: err.message, stack: err.stack });
